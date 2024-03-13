24eb412e23be8c6bb7c47234a8e85bda722b1c4f
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-13 18:13:56 +0100

    Update Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
 import { Header } from './components/Header/Header'
 import { Projects } from './pages/Projects/Projects'
+import { darkModeContext } from './context/Context'
+import { useState } from 'react'
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <darkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </darkModeContext.Provider>
   )
 }
 
