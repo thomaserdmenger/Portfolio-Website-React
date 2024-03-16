591933ecc9a3426adfc6e546ce2ce1f3b3de009e
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-16 21:41:34 +0100

    Add All Projects Page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,3 +1,28 @@
+import "./Projects.css"
+import skills from "../../data/skills"
+import { Link } from "react-router-dom"
+
 export const Projects = () => {
-  return <div>Projects</div>
+  return (
+    <main>
+      <section className='projectPage'>
+        {skills.map((item) => {
+          return (
+            <Link
+              className='projectPage__item'
+              to={`/projects/${item.id}`}
+              key={item.id}>
+              <img src={item.image} alt={item.alt} />
+              <h3>{item.title}</h3>
+              <div className='projectPage__item__skills'>
+                {item.skills.map((skill, index) => {
+                  return <p key={index}>{skill}</p>
+                })}
+              </div>
+            </Link>
+          )
+        })}
+      </section>
+    </main>
+  )
 }
