7fdb2f9af11a8d02b11fb360bc904b50bde40080
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-28 19:00:25 +0100

    Add new Project E-Shop and Change Deployer from Vercel to Render

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,66 +1,76 @@
 const skills = [
   {
     id: 0,
+    title: "Mobile E-Shop App",
+    skills: ["ReactJS", "HTML", "CSS"],
+    features: ["React Router DOM", "Splash Screen", "Fetch API"],
+    image: "/images/project_14.png",
+    alt: "Project",
+    site: "https://e-shop-hmaz.onrender.com",
+    repo: "https://github.com/thomaserdmenger/e-shop"
+  },
+  {
+    id: 1,
     title: "Mobile Tasty Recipe App",
     skills: ["ReactJS", "HTML", "CSS"],
     features: ["React Router DOM", "Splash Screen", "Fetch API"],
     image: "/images/project_3.png",
     alt: "Project",
-    site: "https://tasty-react-sigma.vercel.app/",
+    site: "https://tasty-recipe-react.onrender.com",
     repo: "https://github.com/thomaserdmenger/Tasty-React"
   },
   {
-    id: 1,
+    id: 2,
     title: "Mobile Pokemon App",
     skills: ["ReactJS", "HTML", "CSS"],
     features: ["React Router DOM", "Splash Screen", "Fetch API"],
     image: "/images/project_13.png",
     alt: "Project",
-    site: "https://pokemon-app-gilt-nine.vercel.app/",
+    site: "https://pokemon-app-x9pp.onrender.com",
     repo: "https://github.com/thomaserdmenger/Pokemon-App"
   },
   {
-    id: 2,
+    id: 3,
     title: "Furniture Gallery",
     skills: ["ReactJS", "HTML", "TailwindCSS"],
     features: ["React Router Dom"],
     image: "/images/project_1.png",
     alt: "Project",
-    site: "https://super-code-furniture.vercel.app/",
+    site: "https://supercode-furniture.onrender.com",
     repo: "https://github.com/thomaserdmenger/superCode-Furniture?tab=readme-ov-file"
   },
   {
-    id: 3,
+    id: 4,
     title: "Architect Portfolio",
     skills: ["ReactJS", "HTML", "CSS"],
     features: ["Dark Mode", "React Router DOM", "Splash Screen"],
     image: "/images/project_2.png",
     alt: "Project",
-    site: "https://super-code-architect-portfolio-react.vercel.app/",
+    site: "https://supercode-architect-portfolio-react.onrender.com",
     repo: "https://github.com/thomaserdmenger/superCode-Architect-Portfolio-React?tab=readme-ov-file"
   },
   {
-    id: 4,
+    id: 5,
     title: "Beer Shop",
     skills: ["ReactJS", "HTML", "CSS"],
     features: ["React Router DOM", "Fetch API"],
     image: "/images/project_4.png",
     alt: "Project",
-    site: "https://super-code-beer-api-react-rooter-dom-project.vercel.app/",
+    site: "https://beer-shop-react.onrender.com",
     repo: "https://github.com/thomaserdmenger/superCode-Beer-API-React-Rooter-DOM-Project"
   },
   {
-    id: 5,
+    id: 6,
     title: "Fake Shop",
     skills: ["ReactJS", "HTML", "CSS"],
     features: ["React Router DOM", "Fetch API"],
     image: "/images/project_5.png",
     alt: "Project",
-    site: "https://super-code-fake-store-react-router-3p7d2v6bu.vercel.app/",
+    site: "https://fake-store-react.onrender.com",
     repo: "https://github.com/thomaserdmenger/superCode-Fake-Store-React-Router-DOM?tab=readme-ov-file"
   },
   {
-    id: 6,
+    id: 7,
     title: "Mobile Weather App",
     skills: ["JavaScript", "HTML", "CSS"],
     features: ["Fetch API", "openweathermap.org/api"],
@@ -70,7 +80,7 @@ const skills = [
     repo: "https://github.com/thomaserdmenger/superCode-Weather-App"
   },
   {
-    id: 7,
+    id: 8,
     title: "Resonsive GitHub User Search App",
     skills: ["JavaScript", "HTML", "CSS"],
     features: ["Fetch API", "GitHub REST API", "Dark Mode"],
@@ -80,7 +90,7 @@ const skills = [
     repo: "https://github.com/thomaserdmenger/GitHub-user-search-app"
   },
   {
-    id: 8,
+    id: 9,
     title: "Interior Landing Page",
     skills: ["HTML", "CSS"],
     features: [],
@@ -90,7 +100,7 @@ const skills = [
     repo: "https://github.com/thomaserdmenger/superCode-Interior-Project"
   },
   {
-    id: 9,
+    id: 10,
     title: "Dev Portfolio",
     skills: ["HTML", "CSS"],
     features: ["Parallax Scrolling Effect"],
@@ -100,8 +110,8 @@ const skills = [
     repo: "https://github.com/thomaserdmenger/superCode-Portfolio-Project"
   },
   {
-    id: 10,
-    title: "Dev Portfolio",
+    id: 11,
+    title: "Bon Hotels",
     skills: ["HTML", "SASS"],
     features: [],
     image: "/images/project_10.png",
@@ -110,7 +120,7 @@ const skills = [
     repo: "https://github.com/thomaserdmenger/superCode-Bon-Hotels-Project"
   },
   {
-    id: 11,
+    id: 12,
     title: "Business Payments Page",
     skills: ["HTML", "CSS"],
     features: [],
@@ -120,7 +130,7 @@ const skills = [
     repo: "https://github.com/thomaserdmenger/superCode-Danial-Habib-Project"
   },
   {
-    id: 12,
+    id: 13,
     title: "Travel Website",
     skills: ["HTML", "CSS"],
     features: [],
